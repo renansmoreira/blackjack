d36338a9e32117ff7f6ee371ae50690b37c68710
Renan S Moreira <renan.siravegna@gmail.com>
2024-06-21 13:10:51 +0000

    feat: Allow the backend API to support multiple games with a single instance

--- src/app.js
+++ src/app.js
@@ -1,29 +1,47 @@
 import express from 'express'
 import { v4 as uuidv4 } from 'uuid';
 
-import { Game } from './service/game.js';
+import { Game, GameNullObject } from './service/game.js';
+import { MemoryDatabase } from './infra/database.js';
 
 const app = express()
 const port = 3000
 
-// TODO: Refactor to allow the same API to run multiple games
-const game = new Game(uuidv4())
+const database = new MemoryDatabase()
 
 app.get('/', (_, res) => {
   res.status(200).end()
 })
 
 app.post('/start', (_, res) => {
+  const game = new Game(uuidv4())
   game.start()
+
+  database.addGame(game)
+
   res.json(game)
 })
 
-app.post('/hit', (_, res) => {
+app.post('/hit/:id', (req, res) => {
+  const game = database.findGame(req.params.id)
+
+  if (game instanceof GameNullObject) {
+    res.status(404).send()
+    return
+  }
+
   game.hit()
   res.json(game)
 })
 
-app.post('/stand', (_, res) => {
+app.post('/stand/:id', (req, res) => {
+  const game = database.findGame(req.params.id)
+
+  if (game instanceof GameNullObject) {
+    res.status(404).send()
+    return
+  }
+
   game.stand()
   res.json(game)
 })
