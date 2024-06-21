afd1e04f38e9db176faacf99c0acb9978ea36928
Renan S Moreira <renan.siravegna@gmail.com>
2024-06-21 13:40:56 +0000

    feat: Add express static folder config and remove the root route

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 import express from 'express'
 import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
 
 import { Game, GameNullObject } from './service/game.js';
 import { MemoryDatabase } from './infra/database.js';
 
 const app = express()
+app.use(express.static(path.join(path.resolve(), 'src', 'public')))
 const port = 3000
 
 const database = new MemoryDatabase()
-
-app.get('/', (_, res) => {
-  res.status(200).end()
-})
 
 app.post('/start', (_, res) => {
   const game = new Game(uuidv4())
