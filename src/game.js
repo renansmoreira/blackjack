7fd929eeb3e90a9096fdb67d7dee0431d3f06170
Renan S Moreira <renan.siravegna@gmail.com>
2024-06-21 12:28:02 +0000

    feat: Add a simple object to represent the game state

--- src/game.js
+++ src/game.js
@@ -1,6 +1,25 @@
+import { Dealer } from "./dealer.js"
+import { Player } from "./player.js"
+import { Deck } from "./deck.js"
+import { RoundResult } from "./roundResult.js"
+
 class Game {
+  id = ''
+  dealer = {}
+  player = {}
+  state = RoundResult.CONTINUE
+
+  constructor(id) {
+    this.id = id
+
+    const deck = new Deck()
+    this.dealer = new Dealer(deck)
+    this.player = new Player('Player')
+  }
+
   start() {
-    console.log('Game started')
+    this.dealer.startGame(this.player)
+    console.log('Starting game id:', this.id)
   }
 }
 
--- src/game.spec.js
+++ src/game.spec.js
@@ -1,7 +1,35 @@
+import {jest} from '@jest/globals'
+import { Dealer } from './dealer.js'
 import { Game } from './game.js'
+import { Player } from './player.js'
+import { RoundResult } from './roundResult.js'
 
 describe('Game', () => {
-  it('testing Jest configs', () => {
-    console.log(Game)
+  describe('when constructing', () => {
+    it('should create with the necessary information', () => {
+      const id = 'random-id'
+
+      const game = new Game(id)
+
+      expect(game.id).toEqual(id)
+      expect(game.dealer).toBeInstanceOf(Dealer)
+      expect(game.player).toBeInstanceOf(Player)
+      expect(game.state).toEqual(RoundResult.CONTINUE)
+    })
+  })
+
+  describe('start', () => {
+    it('should ask the dealer to start the game', () => {
+      const game = new Game('random-id')
+      const dealerMock = {
+        startGame: jest.fn()
+      }
+      game.dealer = dealerMock
+
+      game.start()
+
+      expect(dealerMock.startGame).toHaveBeenCalledTimes(1)
+      expect(dealerMock.startGame).toHaveBeenCalledWith(game.player)
+    })
   })
 })
